0cea03d9d22421086359d1eb8e084b65bf4069c8
Sidney Hopson <sidneydhopson@gmail.com>
2023-02-04 14:31:47 -0500

    Add Password Failure UI

--- Assets/Scripts/PasswordFailure.cs
+++ Assets/Scripts/PasswordFailure.cs
@@ -3,30 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BannerUIAnimationScript : MonoBehaviour
-{
 
-    public Image panel;
-    public Text text;
+public class PasswordFailure : MonoBehaviour
+{
+    public Text attemptsText;
+    PasswordSolve ps;
+    int maxAttempts = 3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        attemptsText.text = "Attempts Remaining: " + (0); //set this to (maxAttempts - PasswordSolve.Attempt)
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    IEnumerator transitionIn()
-    {
-        yield return null;
-    }
-
-    IEnumerator transitionOut()
-    {
-        yield return null;
     }
 }
