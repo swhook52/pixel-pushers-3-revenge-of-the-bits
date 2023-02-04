5684e3032d35d3bbfac2762829e3c0187ee61913
Andrew Kiproff <andrewkiproff@gmail.com>
2023-02-04 12:17:53 -0500

    password solve 1

--- Assets/Scripts/PasswordSolve.cs
+++ Assets/Scripts/PasswordSolve.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    int currentTier = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        RootWordsLibrary.LoadRootWords();
+
+        // get a word from the list
+        RootModel word = RootWordsLibrary.RootWords.GetWordByTier(currentTier);
+
     }
 
     // Update is called once per frame
